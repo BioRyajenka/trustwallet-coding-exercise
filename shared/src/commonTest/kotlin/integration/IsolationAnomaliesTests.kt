9933105163d078836d0e68fba249c29ff90d9af1
Igor Sushencev <igor-sushencev@yandex.ru>
2024-08-26 12:10:43 +0400

    Add dirty read, non-repeatable read and phantom read isolation anomalies tests

--- shared/src/commonTest/kotlin/integration/IsolationAnomaliesTests.kt
+++ shared/src/commonTest/kotlin/integration/IsolationAnomaliesTests.kt
@@ -2,24 +2,22 @@ package integration
 
 import com.sushencev.tkvs.KVSBackend
 import com.sushencev.tkvs.OptimisticLockingException
-import com.sushencev.tkvs.Transaction
 import com.sushencev.tkvs.storage.InMemoryStorage
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 
 class IsolationAnomaliesTests {
     @Test
     fun `lost update is not happening`() = runTest {
-        lateinit var txn1: Transaction
-
         // given
         val storage = InMemoryStorage()
         val key = "key"
         storage[key] = "old value"
         val backend = KVSBackend(storage)
 
-        txn1 = backend.beginTransaction()
+        val txn1 = backend.beginTransaction()
         txn1[key] = "value1"
 
         val txn2 = backend.beginTransaction()
@@ -30,5 +28,63 @@ class IsolationAnomaliesTests {
         assertFailsWith(OptimisticLockingException::class) {
             txn1.commit()
         }
+    }
+
+    @Test
+    fun `dirty read is not happening`() = runTest {
+        // given
+        val storage = InMemoryStorage()
+        val key = "key"
+        val oldValue = "old value"
+        storage[key] = oldValue
+        val backend = KVSBackend(storage)
+
+        val txn1 = backend.beginTransaction()
+        txn1[key] = "value1"
+        val txn2 = backend.beginTransaction()
+
+        // when
+        val actual = txn2[key]
+
+        // then
+        assertEquals(oldValue, actual)
+    }
+
+    @Test
+    fun `non-repeatable read is not happening`() = runTest {
+        // given
+        val storage = InMemoryStorage()
+        val key = "key"
+        val oldValue = "old value"
+        storage[key] = oldValue
+        val backend = KVSBackend(storage)
+
+        val txn1 = backend.beginTransaction()
+
+        // when, then
+        val txn2 = backend.beginTransaction()
+        assertEquals(oldValue, txn2[key])
+
+        txn1[key] = "value1"
+        txn1.commit()
+        assertEquals(oldValue, txn2[key])
+    }
+
+    @Test
+    fun `phantom read IS happening`() = runTest {
+        // given
+        val storage = InMemoryStorage()
+        storage["key1"] = "value"
+        val backend = KVSBackend(storage)
+
+        // when, then
+        val txn1 = backend.beginTransaction()
+        txn1["key2"] = "value"
+        assertEquals(2, txn1.count("value"))
+
+        val txn2 = backend.beginTransaction()
+        txn2["key3"] = "value"
+        txn2.commit()
+        assertEquals(3, txn1.count("value"))
     }
 }
